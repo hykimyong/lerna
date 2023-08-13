c08a7e3fe4f2001924ff36faf3019d711a1fbd3a
hykimyong <hykimyong@naver.com>
2023-08-13 18:43:54 +0900

    session설정 추가

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,9 +1,21 @@
 import express,{Application} from 'express';
 import sequelize from './sequelize';
 import routes from './routes';
+import session from 'express-session';
+
+const FileStore = require('session-file-store')(session);
 
 const app: Application = express();
 
+const sessionMiddleWare = session({
+    secret:'cacaonibs',
+    saveUninitialized: true,
+    cookie:{secure:false},
+    resave:false,
+    store:new FileStore(),
+})
+
+app.use(sessionMiddleWare);
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
