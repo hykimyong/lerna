d604e2f1ae525793224b55c27117e580f3961daa
hykimyong <hykimyong@naver.com>
2023-08-14 19:04:14 +0900

    mock생성

--- packages/server/src/routes/user.ts
+++ packages/server/src/routes/user.ts
@@ -1,90 +1,116 @@
 import { Router } from "express";
-import { Op } from "sequelize";
 import { v4 as uuid } from "uuid";
-import Room from "../schemas/room";
+
+import mock from "../mock";
 import User from "../schemas/user";
+import { Op } from "sequelize";
+
 const router = Router();
 
-/**
- * 유저 목록
- */
-router.get('/', async(req,res)=>{
-    try{
-        const result = await User.findAndCountAll({
-            where:{
-                id:{
-                    [Op.ne] : req.session.userId
-                }
-            }
-        });
-        res.json(result);
-    }catch(e){
+/* 유저 목록 */
+router.get("/", async (req, res) => {
+  try {
+    const result = await User.findAndCountAll({
+      where: {
+        id: {
+          // @ts-ignore
+          [Op.ne]: req.session.userId,
+        },
+      },
+    });
 
-    }
+    res.json(result);
+  } catch (e) {}
 });
 
-/**
- * 세션 조회
- */
-router.get('/me', async(req,res)=>{
-    try{
-        const room = await Room.findOne({
-            where : {
-                id : Number(req.params.roomId),
-            },
-            include: User
-        });
-        res.json({
-            username : req.session.username,
-            userId : req.session.userId,
-            isLogged : req.session.isLogged,
-        });
-    }catch(e){
+router.post("/mock", async (req, res) => {
+  try {
+    await User.create({
+      id: uuid(),
+      username: mock[0].username,
+      thumbnailImageUrl: mock[0].thumbnailImageUrl,
+    });
+    await User.create({
+      id: uuid(),
+      username: mock[1].username,
+      thumbnailImageUrl: mock[1].thumbnailImageUrl,
+    });
+    await User.create({
+      id: uuid(),
+      username: mock[2].username,
+      thumbnailImageUrl: mock[2].thumbnailImageUrl,
+    });
 
-    }
+    res.json({
+      statusText: "OK",
+    });
+  } catch (e) {}
 });
 
-/**
- * 로그인
- */
-router.post('/login', async(req,res)=>{
-    try{
-        const userId = uuid();
-        const username = req.body.username;
+/* 유저 생성 */
+router.post("/", async (req, res) => {
+  try {
+    const user = await User.create({
+      id: uuid(),
+      username: req.body.username,
+      thumbnailImageUrl: req.body.thumbnailImageUrl,
+    });
 
-        const user = await User.create({
-            id: userId,
-            username:
-        })
-
-        req.session.username = username;
-        req.session.userId = userId;
-        req.session.isLogged = true;
-
-        req.session.save(()=>{
-            res.json({
-                statusText : 'OK',
-                data : user
-            })
-        })
-    }catch(e){
-
-    }
+    res.json(user);
+  } catch (e) {}
 });
 
-/**
- * 로그아웃
- */
-router.post('/logout',async(req,res)=>{
-    try{
-        delete req.session.user;
+/* 세션 조회 */
+router.get("/me", async (req, res) => {
+  try {
+    res.json({
+      // @ts-ignore
+      username: req.session.username,
+      // @ts-ignore
+      userId: req.session.userId,
+      // @ts-ignore
+      isLogged: req.session.isLogged,
+    });
+  } catch (e) {}
+});
 
-        res.session.save(()=>{
-            res.redirect('/');
-        })
-    }catch(e){
-        
-    }
-})
+/* 로그인 */
+router.post("/login", async (req, res) => {
+  try {
+    const userId = uuid();
+    const username = req.body.username;
+
+    const user = await User.create({
+      id: userId,
+      username,
+    });
+
+    // @ts-ignore
+    req.session.username = username;
+    // @ts-ignore
+    req.session.userId = userId;
+    // @ts-ignore
+    req.session.isLogged = true;
+
+    req.session.save(() => {
+      res.json({
+        statusText: "OK",
+        data: user,
+      });
+    });
+  } catch (e) {}
+});
+
+/* 로그아웃 */
+router.post("/logout", async (req, res) => {
+  try {
+    // @ts-ignore
+    delete req.session.user;
+
+    req.session.save(() => {
+      res.redirect("/");
+    });
+  } catch (e) {}
+});
 
 export default router;
