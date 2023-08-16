521208539219a432243c9c05360650c8bdc5feb0
hykimyong <hykimyong@naver.com>
2023-08-16 12:59:31 +0900

    router-dom 추가

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import {Routes,Route} from 'react-router-dom';
+import Lobby from './pages/Lobby';
+import Friends from './pages/Friends';
+import RoomList from './pages/RoomList';
+import RoomDetail from './pages/RoomDetail';
+import SeeMore from './pages/SeeMore';
 
 function App() {
   return (
-    <div>Hello Cacaonibs !</div>
+    <Routes>
+      <Route index element={<Lobby/>}/> {/** Lobby */}
+      <Route path="/friends" element={<Friends/>}/> {/** Friends */}
+      <Route path="/rooms" element={<RoomList/>}/> {/** RoomList */}
+      <Route path="/rooms/:roomId" element={<RoomDetail/>}/> {/** RoomDetail */}
+      <Route path="/more" element={<SeeMore/>}/> {/** SeeMore */}
+    </Routes>
   );
 }
 
