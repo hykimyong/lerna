a871ac2759266b0b8ef266049e5e115297565b83
hykimyong <hykimyong@naver.com>
2023-08-29 23:38:56 +0900

    roomlist 틀생성

--- packages/client/src/pages/RoomList.tsx
+++ packages/client/src/pages/RoomList.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import styled from "@emotion/styled";
 
-const RoomList: React.FC = () => {
-  return <div>RoomList</div>;
+import BottomNavigation from "../components/BottomNavigation";
+import TopNavigation from "../components/TopNavigation";
+
+const Base = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: 0 12px;
+  box-sizing: border-box;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const RoomListPage: React.FC = () => {
+  return (
+    <Base>
+      <Container>
+        <TopNavigation title="채팅" />
+        <BottomNavigation />
+      </Container>
+    </Base>
+  );
 };
 
-export default RoomList;
+export default RoomListPage;
