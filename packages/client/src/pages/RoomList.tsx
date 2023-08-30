45c0c37970e5382d013d804d7be60295d349c674
hykimyong <hykimyong@naver.com>
2023-08-31 01:26:40 +0900

    리액트쿼리 적용

--- packages/client/src/pages/RoomList.tsx
+++ packages/client/src/pages/RoomList.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import styled from "@emotion/styled";
 
+import ChatRoomList from "../components/ChatRoomList";
+import ChatRoom from "../components/ChatRoomList/ChatRoom";
 import BottomNavigation from "../components/BottomNavigation";
 import TopNavigation from "../components/TopNavigation";
-import ChatRoomList from "../components/ChatRoomList";
+import { useQuery } from "react-query";
+import { AxiosError, AxiosResponse } from "axios";
+import { IRoom } from "../types";
+import { fetchChatRoomList } from "../apis/roomApi";
 
 const Base = styled.div`
   width: 100%;
@@ -18,11 +23,26 @@ const Container = styled.div`
 `;
 
 const RoomListPage: React.FC = () => {
+  const { data: chatRoomListData } = useQuery<
+    AxiosResponse<Array<IRoom>>,
+    AxiosError
+  >("fetchChatRoomList", fetchChatRoomList);
+
   return (
     <Base>
       <Container>
         <TopNavigation title="채팅" />
-        <ChatRoomList/>
+        {chatRoomListData && (
+          <ChatRoomList>
+            {chatRoomListData.data.map((chatRoom) => (
+              <ChatRoom
+                key={chatRoom.id}
+                id={chatRoom.id}
+                username={chatRoom.user.username}
+              />
+            ))}
+          </ChatRoomList>
+        )}
         <BottomNavigation />
       </Container>
     </Base>
