c26675f9d43bfc9768923a462575c8379fef545c
hykimyong <hykimyong@naver.com>
2023-09-02 20:41:03 +0900

    채팅 input 컴포넌트 생성

--- packages/client/src/pages/RoomDetail.tsx
+++ packages/client/src/pages/RoomDetail.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import styled from "@emotion/styled";
+import { Global, css } from "@emotion/react";
 
-const RoomDetail: React.FC = () => {
-  return <div>RoomDetail</div>;
+const globalStyles = css`
+  body {
+    background-color: #abc1d1;
+  }
+`;
+
+const Base = styled.div`
+  position: relative;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 64px;
+  align-items: center;
+  padding: 0 24px;
+`;
+
+const RoomDetailPage: React.FC = () => {
+  return (
+    <Base>
+      <Global styles={globalStyles} />
+      <Container>
+        {/* <MessageList/> */}
+      </Container>
+      {/* <inputChat/> */}
+    </Base>
+  );
 };
 
-export default RoomDetail;
+export default RoomDetailPage;
