47189c1bd8bc1e4a44c710eb6cb11851e848bd35
hykimyong <hykimyong@naver.com>
2023-09-09 16:54:07 +0900

    roomdetail add

--- packages/client/src/pages/RoomDetail.tsx
+++ packages/client/src/pages/RoomDetail.tsx
@@ -1,5 +1,19 @@
+import React, { useRef } from "react";
+import { useParams } from "react-router-dom";
 import styled from "@emotion/styled";
 import { Global, css } from "@emotion/react";
+
+import TopNavigation from "../components/ChatRoomDetail/TopNavigation";
+import InputChat from "../components/ChatRoomDetail/InputChat";
+import MessageList from "../components/ChatRoomDetail/MessageList";
+import { useMutation, useQuery } from "react-query";
+import { fetchChatRoomDetail } from "../apis/roomApi";
+import { AxiosError, AxiosResponse } from "axios";
+import { IChat, IProfile, IRoom } from "../types";
+import { fetchChatMessageList, sendChatMessage } from "../apis/chatApi";
+import SentMessage from "../components/ChatRoomDetail/SentMessage";
+import { fetchMyProfile } from "../apis/userApi";
+import ReceivedMessage from "../components/ChatRoomDetail/ReceiveMessage";
 
 const globalStyles = css`
   body {
@@ -20,13 +34,68 @@ const Container = styled.div`
 `;
 
 const RoomDetailPage: React.FC = () => {
+  const scrollBottomRef = useRef<HTMLLIElement>(null);
+  const { roomId } = useParams<string>();
+
+  const { data: profileData } = useQuery<AxiosResponse<IProfile>, AxiosError>(
+    "fetchMyProfile",
+    fetchMyProfile
+  );
+  const { data: chatRoomDetailData } = useQuery<
+    AxiosResponse<IRoom>,
+    AxiosError
+  >(["fetchChatRoomDetail", roomId], () =>
+    fetchChatRoomDetail(roomId as string)
+  );
+
+  const { data: chatListData } = useQuery<
+    AxiosResponse<Array<IChat>>,
+    AxiosError
+  >(["fetchChatMessageList", roomId], () =>
+    fetchChatMessageList(roomId as string)
+  );
+
+  const mutation = useMutation("sendChatMessage", (content: string) =>
+    sendChatMessage(roomId as string, content)
+  );
+
+  const handleSend = (content: string) => {
+    if (content.length) {
+      mutation.mutate(content);
+    }
+  };
+
   return (
     <Base>
       <Global styles={globalStyles} />
+      {chatRoomDetailData && (
+        <TopNavigation title={chatRoomDetailData.data.user.username} />
+      )}
       <Container>
-        {/* <MessageList/> */}
+        <MessageList>
+          {chatListData?.data.map((message) =>
+            message.senderId === profileData?.data.userId ? (
+              <SentMessage
+                key={message.id}
+                senderId={message.senderId}
+                content={message.content}
+                timestamp={message.createdAt}
+              />
+            ) : (
+              <ReceivedMessage
+                key={message.id}
+                receiver={message.user?.username}
+                receiverThumbnailImage={message.user?.thumbnailImageUrl}
+                senderId={message.senderId}
+                content={message.content}
+                timestamp={message.createdAt}
+              />
+            )
+          )}
+          <li ref={scrollBottomRef} />
+        </MessageList>
       </Container>
-      {/* <inputChat/> */}
+      <InputChat onClick={handleSend} />
     </Base>
   );
 };
